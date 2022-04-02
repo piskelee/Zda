fb75b52e85d0d5af344fbb5c2ed1b654d6600bad
piskelee <58458166+piskelee@users.noreply.github.com>
2022-04-02 20:30:37 +0800

    INIT

--- sprites.py
+++ sprites.py
@@ -25,8 +25,12 @@ class Player(pygame.sprite.Sprite):
         # moving
         self.dir = pygame.math.Vector2()
         self.speed = 5
-
+        # groupy
         self.o_sprites = o_sprites
+        # attack
+        self.attacking = False
+        self.attack_cooldown = 800
+        self.attack_time = None
 
     def input(self):
         keys = pygame.key.get_pressed()
@@ -45,8 +49,21 @@ class Player(pygame.sprite.Sprite):
         else:
             self.dir.y = 0
         # attack
-        if keys[pygame.K_SPACE]:
-            print("attack!!")
+        if keys[pygame.K_SPACE] and not self.attacking:
+            self.attacking = True
+            self.attack_time = pygame.time.get_ticks()
+            print("hit")
+
+        if keys[pygame.K_LCTRL] and not self.attacking:
+            self.attacking = True
+            self.attack_time = pygame.time.get_ticks()
+            print("magic")
+
+    def cooldown(self):
+        current_time = pygame.time.get_ticks()
+        if self.attacking:
+            if current_time - self.attack_time >= self.attack_cooldown:
+                self.attacking = False
 
     def collision(self, dir):
         if dir == "h":
@@ -78,3 +95,4 @@ class Player(pygame.sprite.Sprite):
     def update(self):
         self.input()
         self.move(self.speed)
+        self.cooldown()
